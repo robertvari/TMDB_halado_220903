5d63b9570857cdc04bcf4e5b9189677b4e6f8c6f
Robert Var <mail.robertvari@gmail.com>
2022-09-18 18:09:17 +0200

    Update

--- py_components/movie_list.py
+++ py_components/movie_list.py
@@ -1,16 +1,40 @@
 from PySide2.QtCore import QAbstractListModel, Qt, QModelIndex
+import tmdbsimple as tmdb
+tmdb.API_KEY = '83cbec0139273280b9a3f8ebc9e35ca9'
+tmdb.REQUESTS_TIMEOUT = 5
+
 
 class MovieList(QAbstractListModel):
     DataRole = Qt.UserRole
 
     def __init__(self):
         super().__init__()
+
+        self._movies = []
+        self.tmdb_movies = tmdb.Movies()
+        self._fetch_movies()
+
+    def _fetch_movies(self):
+        self._reset()
         
-        self._movies = [
-            {"title": "Fall", "release_date": "2022-08-11", "vote_average": 74},
-            {"title": "Pinocchio", "release_date": "2022-09-07", "vote_average": 68},
-            {"title": "Beast", "release_date": "2022-08-11", "vote_average": 71}
-        ]
+        popular_movies = self.tmdb_movies.popular(page=1)["results"]
+        for movie_data in popular_movies:
+            self._inser_movie({
+                "title": movie_data.get("title"),
+                "release_date": movie_data.get("release_date"),
+                "vote_average": int(movie_data.get("vote_average") * 10),
+            })
+
+    def _reset(self):
+        self.beginResetModel()
+        self._movies.clear()
+        self.endResetModel()
+
+    def _inser_movie(self, movie_data):
+        self.beginInsertRows(QModelIndex(), self.rowCount(), self.rowCount())
+        self._movies.append(movie_data)
+        self.endInsertRows()
+
 
     def rowCount(self, parent=QModelIndex) -> int:
         return len(self._movies)
