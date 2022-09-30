d5c2e21433133a9956b16927e3b1c2b5bf98fde6
Robert Vári <mail.robertvari@gmail.com>
2022-09-30 09:38:28 +0200

    Correct dates on cards

--- py_components/tmdb_test.py
+++ py_components/tmdb_test.py
@@ -1,4 +1,8 @@
 import tmdbsimple as tmdb
+from datetime import datetime
+# import locale
+# locale.setlocale(locale.LC_TIME, "hu_HU")
+
 tmdb.API_KEY = '83cbec0139273280b9a3f8ebc9e35ca9'
 tmdb.REQUESTS_TIMEOUT = 5
 
@@ -6,7 +10,9 @@ movies = tmdb.Movies()
 popular_movies = movies.popular(page=1)["results"]
 for movie_data in popular_movies:
     title = movie_data.get("title")
-    release_date = movie_data.get("release_date")
+    datetime_obj = datetime.strptime(movie_data.get("release_date"), "%Y-%m-%d")
+    release_date = datetime_obj.strftime("%Y %b %d")
+
     vote_average = int(movie_data.get("vote_average") * 10)
     poster_path = "https://image.tmdb.org/t/p/w300" + movie_data.get("poster_path")
-    print(title, release_date, vote_average, poster_path)
+    print(title, release_date)
