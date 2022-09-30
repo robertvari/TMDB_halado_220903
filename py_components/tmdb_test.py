983573621d35ee5ff21084a272d2d94e3aae4d67
Robert Vári <mail.robertvari@gmail.com>
2022-09-30 10:15:55 +0200

    Test: get movie genres

--- py_components/tmdb_test.py
+++ py_components/tmdb_test.py
@@ -6,13 +6,29 @@ from datetime import datetime
 tmdb.API_KEY = '83cbec0139273280b9a3f8ebc9e35ca9'
 tmdb.REQUESTS_TIMEOUT = 5
 
+genres = tmdb.Genres()
+movie_genres = genres.movie_list()["genres"]
+
+def get_genres(id_list):
+    if not id_list:
+        return []
+
+    result = []
+    for id in id_list:
+        for data in movie_genres:
+            if data["id"] == id:
+                result.append(data["name"])
+
+    return result
+
 movies = tmdb.Movies()
 popular_movies = movies.popular(page=1)["results"]
 for movie_data in popular_movies:
     title = movie_data.get("title")
     datetime_obj = datetime.strptime(movie_data.get("release_date"), "%Y-%m-%d")
     release_date = datetime_obj.strftime("%Y %b %d")
-
     vote_average = int(movie_data.get("vote_average") * 10)
     poster_path = "https://image.tmdb.org/t/p/w300" + movie_data.get("poster_path")
-    print(title, release_date)
+    genres = get_genres(movie_data.get("genre_ids"))
+
+    print(title, genres)
